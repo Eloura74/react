93541ef912d4f24dcca1c3f856c1b376895646ce
faber <faber.quentin@gmail.com>
2025-01-15 15:34:46 +0100

    hirst commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const name = "toto";
+
+  const score = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  const link = [
+    <a key="link1" href="https://vitejs.dev" target="_blank">
+      leink
+    </a>,
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+  ];
+  const person = {
+    name: "toto",
+    age: 10,
+  };
+  const isMajor = true;
+
+  const darkMode = true;
+  // commentaire_________________________________________________________
   return (
-    <>
+    <main
+      style={{
+        backgroundColor: darkMode ? "black" : "white",
+        color: darkMode ? "white" : "black",
+      }}
+    >
+      {/* commentaire */}
       <h1 className="text-3xl font-bold">hello world {name}</h1>
-      <div className="text-3xl">salut</div>
-    </>
+      <div className="text-3xl">
+        {score.map((item, index) => (
+          <span key={index}>{item} </span>
+        ))}
+      </div>
+      <div className="text-3xl">
+        {link.map((item, index) => (
+          <span key={index}>{item} </span>
+        ))}
+      </div>
+      <div className="text-3xl">
+        {person.name} {person.age} ans
+      </div>
+      <div className="text-3xl">
+        Majorité :{isMajor ? " Majeur" : " mineur"}
+      </div>
+      <div className="text-3xl">{darkMode ? "Mode nuit" : "Mode jour"}</div>
+    </main>
   );
 }
 
